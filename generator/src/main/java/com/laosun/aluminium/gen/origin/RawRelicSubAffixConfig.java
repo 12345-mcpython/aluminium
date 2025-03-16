ee791a8fb29edf75d5b507ac0336e3b3bb517638
m1504 <laosun12345@foxmail.com>
2025-03-16 11:22:34 +0800

    :sparkles: features: add export sub_attribute.

---
+++ generator/src/main/java/com/laosun/aluminium/gen/origin/RawRelicSubAffixConfig.java
@@ -0,0 +1,4 @@
+package com.laosun.aluminium.gen.origin;
+
+public record RawRelicSubAffixConfig(int GroupID, int AffixID, String Property, DoubleGetter BaseValue, DoubleGetter StepValue, int StepNum) {
+}
