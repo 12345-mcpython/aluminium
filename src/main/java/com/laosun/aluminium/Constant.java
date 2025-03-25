dad9c31bae0196d7d7cbcd147f95b832af885759
m1504 <laosun12345@foxmail.com>
2025-03-25 19:45:22 +0800

    :sparkles: features: add test relic part 1.

--- src/main/java/com/laosun/aluminium/Constant.java
+++ src/main/java/com/laosun/aluminium/Constant.java
@@ -8,9 +8,13 @@ import java.util.Map;
 
 public final class Constant {
     // <star level> part attribute <base bonus> <double value>
-    public static final Map<String, Map<String, Map<String, Map<String, Double>>>> RELIC_MAIN_ATTRIBUTES;
+    public static final Map<Integer, Map<String, Map<String, Map<String, Double>>>> RELIC_MAIN_ATTRIBUTES;
+    public static final Map<Integer, Map<String, Map<String, Double>>> RELIC_SUB_ATTRIBUTES;
 
     static {
-        RELIC_MAIN_ATTRIBUTES = JSONReader.fromJSON("main_attribute.json", new TypeToken<Map<String, Map<String, Map<String, Map<String, Double>>>>>() {}.getType());
+        RELIC_MAIN_ATTRIBUTES = JSONReader.fromJSON("main_attribute.json", new TypeToken<Map<Integer, Map<String, Map<String, Map<String, Double>>>>>() {
+        }.getType());
+        RELIC_SUB_ATTRIBUTES = JSONReader.fromJSON("sub_attribute.json", new TypeToken<Map<Integer, Map<String, Map<String, Double>>>>() {
+        }.getType());
     }
 }
