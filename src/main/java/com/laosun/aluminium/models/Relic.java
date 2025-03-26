1d2978a5290b1ee7aead2c92f299c5c8543d1f92
m1504 <laosun12345@foxmail.com>
2025-03-26 21:28:00 +0800

    :sparkles: features: add test relic part 2.

--- src/main/java/com/laosun/aluminium/models/Relic.java
+++ src/main/java/com/laosun/aluminium/models/Relic.java
@@ -1,6 +1,7 @@
 package com.laosun.aluminium.models;
 
 import com.laosun.aluminium.Constant;
+import com.laosun.aluminium.exceptions.RelicException;
 import com.laosun.aluminium.utils.MapUtils;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,12 +28,40 @@ public class Relic {
     }
 
     public static Relic createRandomLevelZero(@NotNull Type relicType, int star) {
+        if(star <= 2 || star > 5){
+            throw new RelicException("Star level expected star 2-5 but given " + star);
+        }
         var partValue = Constant.RELIC_MAIN_ATTRIBUTES.get(star).get(relicType.getType());
         var entry = MapUtils.getRandomEntry(partValue);
         var mainAttribute = new Attribute(entry.getKey(), entry.getValue().get("base"));
-        var subAttributes = MapUtils.getRandomList(new ArrayList<>(Constant.RELIC_SUB_ATTRIBUTES.get(star).entrySet()), 3)
-                .stream().map((a) -> new Attribute(a.getKey(), a.getValue().get("base"))).toList();
+        var subCandidates = new ArrayList<>(
+                Constant.RELIC_SUB_ATTRIBUTES.get(star).entrySet()
+        );
+        subCandidates.removeIf(e -> e.getKey().equals(mainAttribute.left()));
+
+        var selectedSubEntries = MapUtils.getRandomList(subCandidates, 3);
+        var subAttributes = selectedSubEntries.stream()
+                .map(e -> new Attribute(e.getKey(), e.getValue().get("base")))
+                .toList();
         return create(relicType, mainAttribute, subAttributes);
+    }
+
+    public static Relic createBySetting(@NotNull Type relicType, Relic.Config setting) {
+        return null;
+    }
+
+    @Getter
+    public static class Config {
+        private final Pair mainAttribute;
+        private final List<Pair> subAttributes;
+
+        public Config(Pair mainAttributeL, List<Pair> subAttributesL) {
+            this.mainAttribute = mainAttributeL;
+            this.subAttributes = subAttributesL;
+        }
+
+        public record Pair(String left, int right) {
+        }
     }
 
     public record Attribute(String left, double right, int promoteLevel) {
