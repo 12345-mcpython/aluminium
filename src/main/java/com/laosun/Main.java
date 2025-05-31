11fa9326bcbc1e80d4990e169ef2f146cefee377
m1504 <laosun12345@foxmail.com>
2025-05-31 13:55:32 +0800

    :bug: bug: add relic suit.

--- src/main/java/com/laosun/Main.java
+++ src/main/java/com/laosun/Main.java
@@ -1,12 +1,9 @@
 package com.laosun;
 
 import com.laosun.aluminium.Battle;
-import com.laosun.aluminium.Constant;
 import com.laosun.aluminium.Queue;
+import com.laosun.aluminium.models.*;
 import com.laosun.aluminium.models.Character;
-import com.laosun.aluminium.models.Enemy;
-import com.laosun.aluminium.models.Moveable;
-import com.laosun.aluminium.models.Relic;
 
 import java.util.List;
 
@@ -33,7 +30,20 @@ public class Main {
         battle.startBattle();
         battle.getQueue().print();
         System.out.println(Relic.createRandomLevelZero(Relic.Type.HAND, 5));
-        String data = "{ \"main_attribute\": { \"crit_attack\": 15 }, \"sub_attributes\": { \"health_percent\": { \"promote_level\": 3, \"attribute_level\": 5 }, \"speed\": { \"promote_level\": 0, \"attribute_level\": 1 }, \"effect_hit_rate\": { \"promote_level\": 1, \"attribute_level\": 2 }, \"breaking_effect\": { \"promote_level\": 0, \"attribute_level\": 0 } } }";
-        System.out.println(Relic.createBySetting(Relic.Type.BODY, 5, 15, data));
+        String hyaBodyJson = "{ \"main_attribute\": { \"crit_attack\": 15 }, \"sub_attributes\": { \"health_percent\": { \"promote_level\": 3, \"attribute_level\": 5 }, \"speed\": { \"promote_level\": 0, \"attribute_level\": 1 }, \"effect_hit_rate\": { \"promote_level\": 1, \"attribute_level\": 2 }, \"breaking_effect\": { \"promote_level\": 0, \"attribute_level\": 0 } } }";
+        String hyaLineJson = "{ \"main_attribute\": { \"energy_regeneration_rate\": 15 }, \"sub_attributes\": { \"defence\": { \"promote_level\": 1, \"attribute_level\": 0 }, \"attack_percent\": { \"promote_level\": 1, \"attribute_level\": 2 }, \"defence_percent\": { \"promote_level\": 1, \"attribute_level\": 2 }, \"effect_resistance\": { \"promote_level\": 1, \"attribute_level\": 0 } } }";
+        String hyaBallJson = "{ \"main_attribute\": { \"health_percent\": 15 }, \"sub_attributes\": { \"health\": { \"promote_level\": 0, \"attribute_level\": 0 }, \"defence\": { \"promote_level\": 2, \"attribute_level\": 2 }, \"crit_chance\": { \"promote_level\": 1, \"attribute_level\": 2 }, \"crit_attack\": { \"promote_level\": 2, \"attribute_level\": 4 } } }";
+        String hyaBootJson = "{ \"main_attribute\": { \"speed\": 15 }, \"sub_attributes\": { \"attack\": { \"promote_level\": 0, \"attribute_level\": 1 }, \"attack_percent\": { \"promote_level\": 1, \"attribute_level\": 2 }, \"effect_resistance\": { \"promote_level\": 1, \"attribute_level\": 1 }, \"breaking_effect\": { \"promote_level\": 2, \"attribute_level\": 1 } } }";
+        String hyaHandJson = "{ \"main_attribute\": { \"attack\": 15 }, \"sub_attributes\": { \"defence\": { \"promote_level\": 1, \"attribute_level\": 1 }, \"health_percent\": { \"promote_level\": 2, \"attribute_level\": 0 }, \"defence_percent\": { \"promote_level\": 0, \"attribute_level\": 2 }, \"speed\": { \"promote_level\": 1, \"attribute_level\": 1 } } }";
+        String hyaHeadJson = "{ \"main_attribute\": { \"health\": 15 }, \"sub_attributes\": { \"attack\": { \"promote_level\": 0, \"attribute_level\": 0 }, \"health_percent\": { \"promote_level\": 1, \"attribute_level\": 3 }, \"defence_percent\": { \"promote_level\": 1, \"attribute_level\": 0 }, \"speed\": { \"promote_level\": 2, \"attribute_level\": 2 } } }";
+        Relic hyaBody = Relic.createBySetting(Relic.Type.BODY, 5, 15, hyaBodyJson);
+        Relic hyaLine = Relic.createBySetting(Relic.Type.LINE, 5, 15, hyaLineJson);
+        Relic hyaBall = Relic.createBySetting(Relic.Type.BALL, 5, 15, hyaBallJson);
+        Relic hyaBoot = Relic.createBySetting(Relic.Type.BOOT, 5, 15, hyaBootJson);
+        Relic hyaHand = Relic.createBySetting(Relic.Type.HAND, 5, 15, hyaHandJson);
+        Relic hyaHead = Relic.createBySetting(Relic.Type.HEAD, 5, 15, hyaHeadJson);
+        RelicSuit hya = new RelicSuit();
+        hya.addMore(hyaBody, hyaLine, hyaBall, hyaBoot, hyaHand, hyaHead);
+        System.out.println(hya.calcTotalValue());
     }
 }
---
+++ src/main/java/com/laosun/aluminium/models/RelicSuit.java
@@ -0,0 +1,67 @@
+package com.laosun.aluminium.models;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RelicSuit {
+    public Relic hand;
+    public Relic head;
+    public Relic body;
+    public Relic boot;
+    public Relic ball;
+    public Relic line;
+    public List<Relic> total = new ArrayList<>();
+
+    public void addToSuit(Relic relic) {
+        switch (relic.relicType) {
+            case Relic.Type.HAND -> hand = relic;
+            case Relic.Type.HEAD -> head = relic;
+            case Relic.Type.BODY -> body = relic;
+            case Relic.Type.BOOT -> boot = relic;
+            case Relic.Type.BALL -> ball = relic;
+            case Relic.Type.LINE -> line = relic;
+        }
+        total.add(relic);
+    }
+
+    public void addMore(Relic... relics) {
+        for (Relic relic : relics) {
+            addToSuit(relic);
+        }
+    }
+
+    public Map<String, Double> calcTotalValue() {
+        Map<String, Double> totalMap = new HashMap<>();
+        if (total == null || total.isEmpty()) {
+            return totalMap;
+        }
+
+        for (Relic relic : total) {
+            if (relic == null) {
+                continue;
+            }
+
+            Relic.Attribute mainAttr = relic.getMainAttribute();
+            if (mainAttr != null) {
+                String mainKey = mainAttr.left();
+                double mainValue = mainAttr.right();
+                totalMap.merge(mainKey, mainValue, Double::sum);
+            }
+
+            List<Relic.Attribute> subAttrs = relic.getSubAttributes();
+            if (subAttrs != null && !subAttrs.isEmpty()) {
+                for (Relic.Attribute subAttr : subAttrs) {
+                    if (subAttr == null) {
+                        continue;
+                    }
+                    String subKey = subAttr.left();
+                    double subValue = subAttr.right();
+                    totalMap.merge(subKey, subValue, Double::sum);
+                }
+            }
+        }
+        return totalMap;
+    }
+}
