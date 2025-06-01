7bb7cf9a858082e2a27e3be2acc2170ba21bff66
m1504 <laosun12345@foxmail.com>
2025-06-01 09:42:04 +0800

    :sparkles: features: add weapon and change encoding to utf8.

--- src/main/java/com/laosun/aluminium/utils/JSONReader.java
+++ src/main/java/com/laosun/aluminium/utils/JSONReader.java
@@ -4,13 +4,14 @@ import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public final class JSONReader {
     public static <T> T fromJSON(String jsonName, Type type) {
         try {
-            String json = Files.readString(Path.of("data/" + jsonName));
+            String json = Files.readString(Path.of("data/" + jsonName), StandardCharsets.UTF_8);
             return new Gson().fromJson(json, type);
         } catch (IOException e) {
             System.out.println("Unable to read resource.");
