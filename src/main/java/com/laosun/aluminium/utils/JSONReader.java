9265ef08201291ec01997b72550e53d1b8cc0fd3
m1504 <laosun12345@foxmail.com>
2025-07-05 18:39:30 +0800

    :sparkles: features: optimize code and add benchmark.

--- src/main/java/com/laosun/aluminium/utils/JSONReader.java
+++ src/main/java/com/laosun/aluminium/utils/JSONReader.java
@@ -1,20 +1,17 @@
 package com.laosun.aluminium.utils;
 
 import com.google.gson.Gson;
+import lombok.SneakyThrows;
 
-import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public final class JSONReader {
+    @SneakyThrows
     public static <T> T fromJSON(String jsonName, Type type) {
-        try {
-            String json = Files.readString(Path.of("data/" + jsonName), StandardCharsets.UTF_8);
-            return new Gson().fromJson(json, type);
-        } catch (IOException e) {
-            throw new RuntimeException("Unable to read resource: " + jsonName, e);
-        }
+        String json = Files.readString(Path.of("data/" + jsonName), StandardCharsets.UTF_8);
+        return new Gson().fromJson(json, type);
     }
 }
