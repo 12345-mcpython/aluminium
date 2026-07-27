a25a0de61b0a0e1066a08f65a0de0a24a4b26b67
m1504 <laosun12345@foxmail.com>
2026-07-27 18:12:23 +0800

    :sparkles: features: remove AI battle code.

--- src/main/java/com/laosun/aluminium/models/CanHit.java
+++ src/main/java/com/laosun/aluminium/models/CanHit.java
@@ -14,7 +14,7 @@ import lombok.ToString;
  */
 @Getter
 @ToString
-public abstract class CanHit implements MoveEvent, KillEvent, AttackEvent {
+public abstract class CanHit {
     /**
      * The display name of this entity.
      */
